377b4fcddc50bd37a613f51e8a152e702f30905b
Carlos Vallejos <cabupy@gmail.com>
2021-01-21 00:35:24 -0300

    Modificamos script

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,5 @@
 #/bin/sh
+cd /home/cabu/redisa-front
 docker build . -t cabupy/redisa-front:latest
+docker image tag cabupy/redisa-front:latest cabupy/redisa-front:latest
+docker image push cabupy/redisa-front:latest
